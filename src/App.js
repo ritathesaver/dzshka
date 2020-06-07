76212a6f58bf00115628dcba29da55129607f273
ritathesaver <49412145+ritathesaver@users.noreply.github.com>
2020-06-07 18:21:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './FirsrComponent';
+import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import NavBar from './NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app-wrapper">
+			<BrowserRouter>
+				<NavBar />
+				<div className="content-wrapper">
+					<div className="component first">
+						<Route path="/first" component={FirstComponent} />
+					</div>
+					<div className="component second">
+						<Route path="/second" component={SecondComponent} />
+						<Redirect from="/second" to="/second/first" />
+					</div>
+					<div className="component third">
+						<Route path="/second/:id" component={ThirdComponent} />
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
