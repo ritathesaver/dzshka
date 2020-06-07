32e1fffdfd2a592f63b15bc4c0276c8b212aa96d
ritathesaver <49412145+ritathesaver@users.noreply.github.com>
2020-06-08 00:59:32 +0300

    Revert "2"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './App.css';
-import FirstComponent from './FirstComponent/FirsrComponent';
-import SecondComponent from './SecondComponent/SecondComponent';
-import ThirdComponent from './ThirdComponent/ThirdComponent';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import NavBar from './Navbar/NavBar';
+import FirstComponent from './FirsrComponent';
+import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import NavBar from './NavBar';
 
 function App() {
 	return (
 		<div className="app-wrapper">
 			<BrowserRouter>
 				<NavBar />
-				<Switch>
-					<Route path="/first" component={FirstComponent} />
-
-					<Route exact path="/second" component={SecondComponent} />
-
-					<Route path="/second/:id" component={ThirdComponent} />
-					<Redirect path="/" to="/second/first" />
-				</Switch>
+				<div className="content-wrapper">
+					<div className="component first">
+						<Route path="/first" component={FirstComponent} />
+					</div>
+					<div className="component second">
+						<Route path="/second" component={SecondComponent} />
+						<Redirect from="/second" to="/second/first" />
+					</div>
+					<div className="component third">
+						<Route path="/second/:id" component={ThirdComponent} />
+					</div>
+				</div>
 			</BrowserRouter>
 		</div>
 	);
--- src/FirsrComponent.jsx
+++ src/FirsrComponent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import '../App.css';
+
 const FirstComponent = () => {
 	return (
-		<div className="component first">
+		<div>
 			<h1>Плачу на техно</h1>
 		</div>
 	);
