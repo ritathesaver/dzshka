c2f30996cbcd07e0368c514f14dcabbb19af9af7
ritathesaver <49412145+ritathesaver@users.noreply.github.com>
2020-06-08 00:30:14 +0300

    2
    second commit

--- src/FirstComponent/FirsrComponent.jsx
+++ src/FirstComponent/FirsrComponent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import '../App.css';
 const FirstComponent = () => {
 	return (
-		<div>
+		<div className="component first">
 			<h1>Плачу на техно</h1>
 		</div>
 	);
